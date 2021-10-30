1a3bbf06e6516546ccb4b74bfa272b4506b04f90
Ryan Broadsword <rbroadsword@gmail.com>
2021-10-29 21:12:55 -0700

    update classes for each word in html

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,78 @@
 $(document).ready(function() {
-  $(".clickable").click(function() {
+  $(".java-click").click(function() {
     $("#java-show").toggle();
     $("#java-hidden").toggle();
+  });
+  $(".op-click").click(function() {
+    $("#op-show").toggle();
+    $("#op-hidden").toggle();
+  });
+  $("").click(function() {
+    $("").toggle();
+    $("").toggle();
+  });
+  $("").click(function() {
+    $("").toggle();
+    $("").toggle();
+  });
+  $("").click(function() {
+    $("").toggle();
+    $("").toggle();
+  });
+  $("").click(function() {
+    $("").toggle();
+    $("").toggle();
+  });
+  $("").click(function() {
+    $("").toggle();
+    $("").toggle();
+  });
+  $("").click(function() {
+    $("").toggle();
+    $("").toggle();
+  });
+  $("").click(function() {
+    $("").toggle();
+    $("").toggle();
+  });
+  $("").click(function() {
+    $("").toggle();
+    $("").toggle();
+  });
+  $("").click(function() {
+    $("").toggle();
+    $("").toggle();
+  });
+  $("").click(function() {
+    $("").toggle();
+    $("").toggle();
+  });
+  $("").click(function() {
+    $("").toggle();
+    $("").toggle();
+  });
+  $("").click(function() {
+    $("").toggle();
+    $("").toggle();
+  });
+  $("").click(function() {
+    $("").toggle();
+    $("").toggle();
+  });
+  $("").click(function() {
+    $("").toggle();
+    $("").toggle();
+  });
+  $("").click(function() {
+    $("").toggle();
+    $("").toggle();
+  });
+  $("").click(function() {
+    $("").toggle();
+    $("").toggle();
+  });
+  $("").click(function() {
+    $("").toggle();
+    $("").toggle();
   });
 });
